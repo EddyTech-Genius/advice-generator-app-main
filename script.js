03b065c46f150901ae410e7c7c6ef6185b091b66
Your Name <you@example.com>
2024-10-27 01:26:22 -0700

    started the js

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
+windows.onload = populateQuote()
+
 let quoteNumber = document.getElementById('quote-num');
 let quoteText = document.getElementById('quote');
+let quoteButton = document.getElementById('quote-button');
 
+quoteButton.addEventListener('click', populateQuote);
 
+function populateQuote() {
+    fetch('https://api.adviceslip.com/advice')
 
-fetch('https://api.adviceslip.com/advice')
+    .then(response => {
+        return response.json()
+    })
+
+    .then(data => {
+        let name = data.slip
+
+        quoteNumber.innerHTML = name.id
+        quoteText.innerHTML = name.advice
+    })
+}
